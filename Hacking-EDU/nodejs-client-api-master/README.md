3033061bcc5b51fb03d2a2ddb231eebe29ab3745
Devashish Badlani <deebee2307@gmail.com>
2015-10-24 06:44:49 -0400

    Commit Cloud Database
    Commit Cloud Database

---
+++ Hacking-EDU/nodejs-client-api-master/README.md
@@ -0,0 +1,24 @@
+#nodejs-client-api#
+==============
+
+##Clusterpoint Node.js API##
+
+* [Clusterpoint WEB page](https://www.clusterpoint.com)
+
+* [Clusterpoint Documentation page](https://www.clusterpoint.com/docs/)
+
+* [Clusterpoint API library reference and examples](https://www.clusterpoint.com/docs/?page=Reference)
+
+* [Clusterpoint tutorials](https://www.clusterpoint.com/docs/?page=Tutorials)
+
+* [NPM package manager](https://www.npmjs.org/package/cps-api)
+
+##Installation##
+
+Use NPM package manager to install Clusterpoint Node.js API:
+
+`npm install cps-api`
+
+##Support##
+Please feel free to contact the maintainer of this package on
+support@clusterpoint.com
--- Hacking-EDU/routes/login.js
+++ Hacking-EDU/routes/login.js
@@ -4,9 +4,7 @@ exports.login = function(req, res){
 	var u=req.param("email");
      var p=req.param("password");
 
-		var m=require("mysql");
-	
-	var c=m.createConnection({
+	/*var c=m.createConnection({
 		host: 'localhost',
 		user: 'root',
 		password: '',
@@ -15,10 +13,38 @@ exports.login = function(req, res){
 		multipleStatements :true
 	});
 	
-	
-	c.connect();
- var t=c.query("select count(*) as count from student where s_email='"+(u)+"' and s_password='"+(p)+"'",function(err,result){
-	//console.log(t.sql);	
+	*/
+		
+		//Includes
+		var cps = require('cps-api');
+
+		//Creating a CPS connection
+		var cpsConn = new cps.Connection(  'tcps://cloud-us-0.clusterpoint.com:9008', 'SwagData', 'deebee2307@gmail.com', 'Deebee07..', 'document', 'document/id', {account: 102241});
+
+		// Debug
+		//cpsConn.debug = true;
+		// Search for documents containing word "USA" in any of the fields AND field Population value > 25000000
+		var query = cps.Term("db", "s_password") + cps.Term("db@cluster.com", "s_email");
+		// Do not list fields "Lat" and "Long" from the documents
+		var list = {"Lat": "no", "Long": "no"};
+		// Return 10 documents.
+		var docs = 10;
+		// Return results starting from the 0 position - that is beginning.
+		var offset = 0;
+		// Search request and order results by City_Urban_area field in ascending alphabetical order.
+		var searchRequest = new cps.SearchRequest(query, offset, docs, list);
+
+		
+		cpsConn.sendRequest(searchRequest, function (err, searchResponse) {
+			  if (err) return console.log(err);
+			  if (searchResponse.hits > 0) {
+			    console.log("Found " + searchResponse.hits + " documents");
+			    console.log("showing from " + searchResponse.from + " to " + searchResponse.to + "");
+			  }
+		})
+		//cpsConn.connect();
+// var t=c;psConn.query("select count(*) as count from student where s_email='"+(u)+"' and s_password='"+(p)+"'",function(err,result){
+ /*console.log(t.sql);	
 
 		if(err){
 			console.error(err);
@@ -32,5 +58,6 @@ exports.login = function(req, res){
 	 	 
 	 }
  });
+ */
 };
 
